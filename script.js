c81929279e81c29958294bbd9ee7764eea72452a
thpa0001 <thpa0001@stud.kea.dk>
2025-06-06 15:14:18 +0200

    forside, om mig done

---
+++ script.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+menu.addEventListener("click", menuClick);
+function menuClick() {
+  burger.classList.remove("active");
+  nav.classList.remove("active");
+}
